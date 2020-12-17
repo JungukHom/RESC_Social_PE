0079491939baf7c03e5e525923f6e9ba37be4035
JungukHom <developer0223@gmail.com>
2020-12-17 17:27:59 +0900

    Edit | Add steamvr and swimmingpool asset

---
+++ Assets/1. Assets/Vive-Teleporter/Scripts/EventTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+public class EventTest : MonoBehaviour
+{
+    public Action callback = null;
+
+    public void TriggerEnter()
+    {
+        Vector3 _position = transform.position;
+        _position.y = GameObject.FindGameObjectWithTag("Player").transform.position.y;
+        GameObject.FindGameObjectWithTag("Player").transform.position = _position;
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        if (other.transform.tag.Equals("Player"))
+        {
+            other.transform.position = new Vector3(transform.position.x, other.transform.position.y, transform.position.z);
+            gameObject.SetActive(false);
+        }
+       
+    }
+}
---
+++ Assets/2. Scripts/ReturnObject.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnObject : MonoBehaviour
+{
+    public float waitTime = 2.0f;
+
+    Coroutine coroutine = null;
+
+    Rigidbody objRigidbody = null;
+    Transform objTranform = null;
+
+    Vector3 myOriginPosition = Vector3.zero;
+    Quaternion myOriginRotation = Quaternion.identity;
+
+    private void Start()
+    {
+        objRigidbody = GetComponent<Rigidbody>();
+        objTranform = GetComponent<Transform>();
+
+        myOriginPosition = objTranform.position;
+        myOriginRotation = objTranform.rotation; 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("GabableObject"))
+        {
+            if (coroutine == null)
+            {
+                coroutine = StartCoroutine(ObjectReturn());
+            }
+           
+        }
+    }
+
+    IEnumerator ObjectReturn()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        objRigidbody.velocity = Vector3.zero;
+        objRigidbody.angularVelocity = Vector3.zero;
+
+        objTranform.position = myOriginPosition;
+        objTranform.rotation = myOriginRotation;
+
+        coroutine = null;
+
+        yield return null;
+    }
+}
