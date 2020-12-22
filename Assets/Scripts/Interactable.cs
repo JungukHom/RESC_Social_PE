576e4d351816642980ff79b13e919302b388ac1c
JungukHom <developer0223@gmail.com>
2020-12-22 12:43:23 +0900

    Add | Scene 01(making), 03 & VR

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,19 @@
+﻿namespace RESC
+{
+    // C#
+    using System.Collections;
+    using System.Collections.Generic;
+
+    // Unity
+    using UnityEngine;
+
+    // Project
+    // Alias
+
+    [RequireComponent(typeof(Rigidbody))]
+    public class Interactable : MonoBehaviour
+    {
+        [HideInInspector]
+        public Hand m_ActiveHand = null;
+    }
+}
